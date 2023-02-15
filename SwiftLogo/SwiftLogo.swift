e589e2530244c8a557dcada1c902df37ac644881
Tahe <taha.bensaid132002@gmail.com>
2023-02-15 16:17:11 +0100

    Swift Logo Creation With SwiftUI

---
+++ SwiftLogo/SwiftLogo.swift
@@ -0,0 +1,93 @@
+//
+//  ContentView.swift
+//  SwiftLogo
+//
+//  Created by tahe on 15/2/2023.
+//
+
+import SwiftUI
+
+struct SwiftLogo: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        let width = rect.width
+        let height = rect.height
+        
+        let startPoint = CGPoint(x: rect.minY, y: height * 0.63)
+        
+        path.move(to: startPoint)
+        path.addCurve(
+            to: CGPoint(x: width * 0.98, y: height * 0.98),
+            control1: CGPoint(x: width * 0.5, y: height * 1.35),
+            control2: CGPoint(x: width * 0.85, y: height * 0.67)
+        )
+        
+        path.addQuadCurve(
+            to: CGPoint(x: width * 0.9, y: height * 0.7),
+            control: CGPoint(x: width * 1.0, y: height * 0.85)
+        )
+        
+        path.addQuadCurve(
+            to: CGPoint(x: width * 0.61, y: rect.minY),
+            control: CGPoint(x: width * 1.0, y: height * 0.3)
+        )
+        
+        path.addQuadCurve(
+            to: CGPoint(x: width * 0.7, y: height * 0.53),
+            control: CGPoint(x: width * 0.78, y: height * 0.3)
+        )
+        
+        path.addQuadCurve(
+            to: CGPoint(x: width * 0.215, y: height * 0.09),
+            control: CGPoint(x: width * 0.35, y: height * 0.25)
+        )
+        
+        path.addQuadCurve(
+            to: CGPoint(x: width * 0.5, y: height * 0.48),
+            control: CGPoint(x: width * 0.3, y: height * 0.25)
+        )
+        
+        path.addQuadCurve(
+            to: CGPoint(x: width * 0.1, y: height * 0.15),
+            control: CGPoint(x: width * 0.35, y: height * 0.4)
+        )
+        
+        path.addQuadCurve(
+            to: CGPoint(x: width * 0.56, y: height * 0.71),
+            control: CGPoint(x: width * 0.33, y: height * 0.51)
+        )
+        
+        path.addQuadCurve(
+            to: startPoint,
+            control: CGPoint(x: width * 0.3, y: height * 0.85)
+        )
+        return path
+    }
+    
+}
+
+struct SwiftLogo_Previews: PreviewProvider {
+    static var previews: some View {
+        // Now Let's Add Some Colors
+        ZStack {
+            RoundedRectangle(cornerRadius: 100)
+                .fill(
+                    LinearGradient(
+                        colors: [
+                            Color(red: 253.0/255, green: 38.0/255, blue: 33.0/255),
+                            Color(red: 248.0/255, green: 137.0/255, blue: 54.0/255)
+                        ],
+                        startPoint:  .bottomTrailing,
+                        endPoint: .topLeading
+                    )
+                )
+            SwiftLogo()
+                .fill(.white)
+                .padding(100)
+            
+        }
+        .padding(50)
+        .aspectRatio(1,contentMode: .fit)
+    }
+}
---
+++ SwiftLogo/SwiftLogoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLogoApp.swift
+//  SwiftLogo
+//
+//  Created by tahe on 15/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLogoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AnimatedSwiftLogo()
+        }
+    }
+}
